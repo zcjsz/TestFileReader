e5684a4fa5f9c76c4a584aa3a6c9a5ecdcf31d05
ghfan <ghfan@bmw001>
2018-09-14 15:39:05 +0800

    add componenthash class

---
+++ src/kdf/reader/ComponentHash.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kdf.reader;
+
+/**
+ *
+ * @author ghfan
+ */
+public
+	class ComponentHash {
+	private
+		String comClass = null;
+	private
+		String comName = null;
+
+
+	/**
+	 * 
+	 * @param comClass
+	 * @param comName 
+	 */
+	public
+	ComponentHash(String comClass, String comName) {
+		this.comClass = comClass;
+		this.comName = comName;
+	}
+
+	public
+	String getComClass() {
+		return comClass;
+	}
+
+	public
+	String getComName() {
+		return comName;
+	}
+	
+	
+}
